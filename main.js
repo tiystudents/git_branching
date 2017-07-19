e77fbd785ba6e0633bddc2a5b987e149e0dad903
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-19 11:27:50 -0400

    commented out secondsLeft

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let intervalID = window.setInterval(subtractFromCountdown, tick)
 
 btn.addEventListener("click", function handleResetClick(event) {
   console.log(secondsLeft);
-  secondsLeft = 3;
+  //secondsLeft = 3;
   updateDisplay(secondsLeft);
   let body = document.querySelector("body")
   body.style.backgroundColor = "white";
